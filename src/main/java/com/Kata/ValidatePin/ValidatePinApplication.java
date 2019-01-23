aac88369526d2e27b2f338d433c01bc3ed2caa14
Phil Thomas <philip.thomas@thoughtworks.com>
2019-01-23 13:24:59 -0500

    removed unwanted package declaration

--- src/main/java/com/Kata/ValidatePin/ValidatePinApplication.java
+++ src/main/java/com/Kata/ValidatePin/ValidatePinApplication.java
@@ -1,8 +1,5 @@
 package com.Kata.ValidatePin;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
